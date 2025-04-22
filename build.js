2c4b2706e92f4532857fe05b6396e1901988ef27
袁炜海 <yuanweihai25@163.com>
2025-04-22 23:51:15 +0800

    增加打包流程

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 
-import { emptyDir, copy } from 'fs-extra'
+import { emptyDir, copy, remove } from 'fs-extra'
 import { join } from 'node:path';
+import { exec } from 'node:child_process'
 
 const root = process.cwd();
 
@@ -9,4 +10,10 @@ const root = process.cwd();
     await copy(join(root, 'images'), join(root, 'dist/images'));
     await copy(join(root, 'scripts'), join(root, 'dist/scripts'));
     await copy(join(root, 'manifest.json'), join(root, 'dist/manifest.json'));
+
+    await remove(join(root, 'dist.zip'));
+
+    // only for macOS
+    exec('zip -r dist.zip dist');
+
 })()
